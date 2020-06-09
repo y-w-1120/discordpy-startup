92688d7d7fe14898eceb1881a7eacd41c75d3c71
y-w-1120 <66637903+y-w-1120@users.noreply.github.com>
2020-06-09 19:26:18 +0900

    added nyan

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,10 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+    
+@bot.command()
+async def nyan(ctx):
+    await ctx.send('にゃーん')
+
 
 bot.run(token)
