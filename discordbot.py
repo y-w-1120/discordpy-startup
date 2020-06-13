7c9f1a9dd7a6c81f55c611ac2ab542fce0edaffc
y-w-1120 <66637903+y-w-1120@users.noreply.github.com>
2020-06-13 17:10:32 +0900

    new bot command added /team /team_norem /group

--- discordbot.py
+++ discordbot.py
@@ -23,4 +23,27 @@ async def nyan(ctx):
     await ctx.send('にゃーん')
 
 
+# メンバー数が均等になるチーム分け
+@bot.command()
+async def team(ctx, specified_num=2):
+    make_team = MakeTeam()
+    remainder_flag = 'true'
+    msg = make_team.make_party_num(ctx,specified_num,remainder_flag)
+    await ctx.channel.send(msg)
+
+# メンバー数が均等にはならないチーム分け
+@bot.command()
+async def team_norem(ctx, specified_num=2):
+    make_team = MakeTeam()
+    msg = make_team.make_party_num(ctx,specified_num)
+    await ctx.channel.send(msg)
+
+# メンバー数を指定してチーム分け
+@bot.command()
+async def group(ctx, specified_num=1):
+    make_team = MakeTeam()
+    msg = make_team.make_specified_len(ctx,specified_num)
+    await ctx.channel.send(msg)
+
+
 bot.run(token)
